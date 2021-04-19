4b4e5c117698362ff4269570deab35b9b77ca81e
Justinas Grigas <jstn_as@protonmail.com>
2021-04-19 01:24:10 +0100

    Even better colours

--- config.def.h
+++ config.def.h
@@ -3,9 +3,9 @@ static const char *user  = "iota";
 static const char *group = "iota";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "#1f1f2f", // after initialization
-	[INPUT] =  "#9f7f4f", // during input
-	[FAILED] = "#9f4f5f", // wrong password
+	[INIT] =   "#0f1f2f", // after initialization
+	[INPUT] =  "#9f6f4f", // during input
+	[FAILED] = "#9f4f6f", // wrong password
 };
 
 /* treat a cleared input like a wrong password (color) */
